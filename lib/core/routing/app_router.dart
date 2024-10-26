c099edc9c432e3c2d9607da7ca4fab273f842698
Hazem <hzm@gmail.com>
2024-10-26 16:42:40 +0300

    Update Transition Animations and Project Status Container Border Color
    Replace `CustomZoomingTransition` with `CustomSliderTransition` for project and task details routes in `AppRouter`. This change aligns the transition animations with the desired sliding effect, enhancing the user experience.
    
    Additionally, update the border color of the `ProjectStatusContainer` widget from a grey shade to white. This modification improves the visual consistency and readability of the project status display.

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -2,7 +2,6 @@ import 'package:client_app/core/routing/routes.dart';
 import 'package:client_app/core/views/project_details_view.dart';
 import 'package:client_app/core/views/task_details_view.dart';
 import 'package:client_app/core/widgets/custom_slider_transition.dart';
-import 'package:client_app/core/widgets/custom_zooming_transition.dart';
 import 'package:client_app/features/authentication/ui/login_view.dart';
 import 'package:client_app/features/authentication/ui/sign_up_view.dart';
 import 'package:client_app/features/bottom_nav_bar_switcher/ui/switcher_view.dart';
@@ -67,7 +66,7 @@ class AppRouter {
       // project details
       GoRoute(
         path: Routes.projectDetailsView,
-        pageBuilder: (context, state) => CustomZoomingTransition(
+        pageBuilder: (context, state) => CustomSliderTransition(
           child: const ProjectDetailView(),
           key: state.pageKey,
           duration: 300,
@@ -76,7 +75,7 @@ class AppRouter {
       // task details
       GoRoute(
         path: Routes.taskDetailsView,
-        pageBuilder: (context, state) => CustomZoomingTransition(
+        pageBuilder: (context, state) => CustomSliderTransition(
           child: const TaskDetailsView(),
           key: state.pageKey,
           duration: 300,
